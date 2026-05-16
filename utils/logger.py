480b5ad7f4f6cc454254090b453b24956fa2f7bb
DreamY1314 <a619215878@gmail.com>
2026-05-16 13:17:08 +0800

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,17 @@
+import logging
+import sys
+
+
+def setup_logger(name: str, level: int = logging.INFO) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(
+            logging.Formatter(
+                fmt="%(asctime)s [%(levelname)s] %(message)s",
+                datefmt="%H:%M:%S",
+            )
+        )
+        logger.addHandler(handler)
+    logger.setLevel(level)
+    return logger
